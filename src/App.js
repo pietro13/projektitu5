c1487722eff2a391c8348dac07c74056de3f2be8
Patrik Pricl <patrik.pricl@centrum.sk>
2020-11-13 21:28:29 +0100

    git config --global user.email "patrik.pricl@centrum.sk" git config --global user .name "Patrik Pricl"

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ITU from './ITU.js';
+
+function App() {
+  return (
+    <div className="App">
+      <ITU/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ITU.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class ITU extends Component {
+    render() {
+        return(
+            <div className="ITU">
+                ITU
+            </div>
+        );
+
+    }
+}
+
+export default ITU;
